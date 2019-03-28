03635ff48ff08302fea1fc3044e9b09f1df24b84
Dhiraj Saharia <dsaharia@outlook.com>
2019-03-29 01:06:32 +0530

    added m/c representation

--- 04_object_code_new.c
+++ 04_object_code_new.c
@@ -10,27 +10,28 @@ int main() {
 	fmnemonic = fopen("mnemonic.txt", "r");
 
 	char line[1000], operand[20], opcode[5], object_code[30], mnemonic[25], tabbed_mnemonic[25], table_mnemonic[25], location[25], table_label[25];
-	char target_label[10];
+	char target_label[10], target_operand[10];
+	int index_location;
 
 	while(fgets(line,1000, fintermediate)) {
 		sscanf(line, "%*s %s %s %s", tabbed_mnemonic, mnemonic, operand);
 		printf("%s %s %s\n", tabbed_mnemonic, mnemonic, operand);
 		line[strlen(line) - 1] = ' ';
+		// if (mnemonic[strlen(mnemonic)-1] == 'X') {
+		// 	strcpy(target_address, )
+		// }
 
 		while (fscanf(fmnemonic, "%s\t%s", table_mnemonic, object_code) == 2) {
 			if(!strcmp(table_mnemonic, mnemonic) || !strcmp(table_mnemonic, tabbed_mnemonic)) {
 				if (!strcmp(mnemonic, "LDX")) {
 					strcpy(target_label, operand);
 					printf("TARGET: %s\n", target_label);
-					rewind(fintermediate);
 				}
 				strcat(line, object_code);
 				break;
 			}
 		 // for mnemonic to opcode
 		}
-		
-
 
 		while(fscanf(fsymbol, "%s %s", location, table_label) == 2) {
 			if (line[5] == '\t') {
@@ -64,5 +65,17 @@ int main() {
 		fputc('\n', fobject);
 		
 	 }
+	 
+	while (fgets(line,1000, fintermediate)) {
+		sscanf(line, "%*s %s %s %s", tabbed_mnemonic, mnemonic, operand);
+		if (!strcmp(target_label, tabbed_mnemonic)) {
+			index_location = atoi(operand);
+			break;
+		}
+		// while (fscanf(fsymbol, "%s %s", location, table_label) == 2) {
+		// 	if (!strcmp())
+		// }
+	}
+	printf("value at register X: %d\n", index_location);
 
 }
